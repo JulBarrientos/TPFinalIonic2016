025d11808c2c5dc1dd34922c978ccb15a0bc5369
julbarrientos <julbarrientos@gmail.com>
2017-02-05 17:07:50 -0300

    maqueta incial

---
+++ www/js/controllers.js
@@ -0,0 +1,84 @@
+angular.module('app.controllers', [])
+
+
+.factory("Batallas", function($firebaseArray) {
+  var infosRef = new Firebase("https://batalla-naval-de-la-muerte.firebaseio.com/Batallas");
+  return $firebaseArray(infosRef);
+})
+  
+.controller('homeCtrl', ['$scope', '$stateParams','Batallas', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams,Batallas) {
+    console.log(Batallas);
+    $scope.info =Batallas;
+    
+
+
+}])
+   
+.controller('cartCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cloudCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('batallaCtrl', ['$scope', '$stateParams','Batallas', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams,Batallas) {
+        console.log(Batallas);
+        $scope.botonPrueba = function(){
+        console.log("Hola");
+          Batallas.$add({
+                           "jugador1":[0,0,0,0],
+                           "jugador2":[0,0,0,0],
+                           "acertoJ1":false,
+                           "acertoJ2":false
+                          });      
+    }
+
+}])
+   
+.controller('creditosCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('perfilCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
